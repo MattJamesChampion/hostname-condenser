cd6fd979827083e0cb7ecaaa756521dd2dec7708
Matthew Champion <mattjameschampion@gmail.com>
2018-01-27 15:04:31 +0000

    Add condense_hostname function framework and docstring

--- hostname_condenser.py
+++ hostname_condenser.py
@@ -8,3 +8,9 @@ moving part of the full hostname into the top-level directory (TLD).
 An example is if the hostname was "egg": this could also be represented as
 "e.gg", which would form part of a larger URI.
 """
+
+
+def condense_hostname():
+    """Takes a hostname and an optional list of top-level domains and generates
+    possible hostname-TLD combinations
+    """
