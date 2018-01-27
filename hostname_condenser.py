cfd7418c65b55c898d7cfd5b6838f5eeda11a990
Matthew Champion <mattjameschampion@gmail.com>
2018-01-27 15:01:02 +0000

    Add a docstring for the hostname_condenser module

--- hostname_condenser.py
+++ hostname_condenser.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+"""A module for generating a list of condensed hostnames.
+
+A module for generating a list of condensed hostnames, where condensing is
+moving part of the full hostname into the top-level directory (TLD).
+
+An example is if the hostname was "egg": this could also be represented as
+"e.gg", which would form part of a larger URI.
+"""
