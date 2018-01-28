cce7db2a5563022a5800267862ce5bec2ea074e7
Matthew Champion <mattjameschampion@gmail.com>
2018-01-28 17:02:25 +0000

    Add test_get_default_top_level_domains_returns_results

--- test_hostname_condenser.py
+++ test_hostname_condenser.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 
-import hostname_condenser
+from hostname_condenser import get_default_top_level_domains
 
 class TestHostnameCondenser(TestCase):
-    pass
-
+    def test_get_default_top_level_domains_returns_results(self):
+        top_level_domains = get_default_top_level_domains()
+        
+        self.assertNotEqual(top_level_domains, None)
