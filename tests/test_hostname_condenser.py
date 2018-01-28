3b0ec289add94d8cddfeb6c6a4ae6afc867caf2f
Matthew Champion <mattjameschampion@gmail.com>
2018-01-28 17:12:59 +0000

    Add test_get_top_level_domains_returns_expected_results

--- tests/test_hostname_condenser.py
+++ tests/test_hostname_condenser.py
@@ -1,8 +1,17 @@
 from unittest import TestCase
 
-from hostname_condenser import get_default_top_level_domains
+from hostname_condenser import (get_top_level_domains,
+                                get_default_top_level_domains)
 
 class TestHostnameCondenser(TestCase):
+    def test_get_top_level_domains_returns_expected_results(self):
+        file_path = r"tests\test_data\example_tlds.txt"
+        actual_top_level_domains = get_top_level_domains(file_path)
+        
+        expected_top_level_domains = ["test", "tld", "other", "words"]
+        
+        self.assertEqual(expected_top_level_domains, actual_top_level_domains)
+    
     def test_get_default_top_level_domains_returns_results(self):
         top_level_domains = get_default_top_level_domains()
         
